2c34afe4569ed8bd46f74e5ed07fd11aff960b8a
Rico <richardthomaswhitfield@gmail.com>
2017-02-10 17:17:14 -0600

    getting random q back

--- jeopardy.py
+++ jeopardy.py
@@ -1,3 +1,11 @@
-import request
+import requests
+import random
 
 def jeopardy():
+	random_jeopardy_key = random.randrange(9000,10000)
+	r = requests.get('https://jeopartymode.firebaseio.com/{}.json'
+					.format(random_jeopardy_key))
+
+	print(r.json())
+
+jeopardy()
