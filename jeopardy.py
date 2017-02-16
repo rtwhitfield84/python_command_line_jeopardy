fcbe5c52c70d6a78d8b21f54811a652cd0882857
Rico <richardthomaswhitfield@gmail.com>
2017-02-15 22:01:02 -0600

    updated some gameplay

--- jeopardy.py
+++ jeopardy.py
@@ -1,6 +1,7 @@
 import requests
 import random
 import parser
+import os
 
 
 
@@ -13,22 +14,37 @@ def jeopardy():
 	print("\n""********************** \n"  
 		"Command Line Jeopardy! \n" 
 		"**********************")
+	print("Enter q to quit")
 
 	for k,v in r.json().items():
 		if str(k) == 'answer':
 			answer = v.lower()
 		if str(k) == 'question':
-			parser.strip_tags(v)
 			question = v
+			parser.strip_tags(question)
 		if str(k) == 'category':
 			category = v
 		if str(k) == 'value':
 			value = v
 		if str(k) == 'round':
 			j_round = v
-		print(k,v+'\n')
+			
+	print(j_round + '\n')
+	print('Category \n' + category + '\n')
+	print('Question \n' + question + '\n')
 
 	user_answer = str(input('What is: ')).lower()
 	if user_answer == answer:
+		os.system('clear')
 		print('correct')
-jeopardy()
+		jeopardy()
+	elif user_answer == 'q':
+		print('Goodbye')
+		exit()
+	else:
+		os.system('clear')
+		print("incorrect")
+		jeopardy()
+
+if __name__ == '__main__':
+	jeopardy()
